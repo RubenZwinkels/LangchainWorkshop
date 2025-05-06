118cf1e68d621916cbea44c4f207bcfe356c3a60
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-06 13:38:31 +0200

    model aanmaken

--- src/main/java/nl/infosupport/swagshop/Main.java
+++ src/main/java/nl/infosupport/swagshop/Main.java
@@ -1,8 +1,19 @@
 package nl.infosupport.swagshop;
 
 
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+
 public class Main {
     public static void main(String[] args) {
+        ChatModel model = OpenAiChatModel.builder()
+                .modelName(OpenAiChatModelName.O3_MINI)
+                .apiKey("")
+                .build();
 
+        var answer = model.chat("Hello World! Greetings from Info Support!");
+
+        System.out.println(answer);
     }
 }
