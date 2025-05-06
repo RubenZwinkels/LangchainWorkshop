2dfd72517508afcb653a0be66477890be0ee00f0
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-06 13:49:43 +0200

    in main chatten met bot

--- src/main/java/nl/infosupport/swagshop/Main.java
+++ src/main/java/nl/infosupport/swagshop/Main.java
@@ -5,6 +5,8 @@ import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ChatModel model = OpenAiChatModel.builder()
@@ -12,8 +14,17 @@ public class Main {
                 .apiKey("")
                 .build();
 
-        var answer = model.chat("Hello World! Greetings from Info Support!");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(answer);
+        while (true) {
+            System.out.print("Jij: ");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("exit")) break;
+
+            var answer = model.chat(input);
+            System.out.println("Bot: " + answer);
+        }
+
+        scanner.close();
     }
 }
